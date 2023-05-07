5087fa275ded212d2df37583e5e96c9f62120627
Leo Spin <leochang658@gmail.com>
2023-05-07 19:50:05 +0800

    Hotel data

--- app/src/main/java/TourismWiz/TourismWiz/data/AppContainer.kt
+++ app/src/main/java/TourismWiz/TourismWiz/data/AppContainer.kt
@@ -1,5 +1,6 @@
 package TourismWiz.TourismWiz.data
 
+import TourismWiz.TourismWiz.network.HotelApiService
 import TourismWiz.TourismWiz.network.RestaurantApiService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -8,6 +9,7 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val restaurantRepository : RestaurantRepository
+    val hotelRepository : HotelRepository
 }
 
 class DefaultAppContainer : AppContainer{
@@ -18,11 +20,21 @@ class DefaultAppContainer : AppContainer{
         .baseUrl(BASE_URL)
         .build()
 
+    /* Restaurant */
     private val retrofitRestaurantService : RestaurantApiService by lazy {
         retrofit.create(RestaurantApiService::class.java)
     }
 
     override val restaurantRepository: RestaurantRepository by lazy {
         NetworkRestaurantRepository(retrofitRestaurantService)
+    }
+
+    /* Hotel */
+    private val retrofitHotelService : HotelApiService by lazy{
+        retrofit.create(HotelApiService::class.java)
+    }
+
+    override val hotelRepository: HotelRepository by lazy {
+        NetworkHotelRepository(retrofitHotelService)
     }
 }
