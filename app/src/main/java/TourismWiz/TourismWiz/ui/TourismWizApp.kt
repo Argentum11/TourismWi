4ff8dd8b6b87e56f5d7b78544e66284ce2851146
Chris Tang <tangchris24@gmail.com>
2023-05-19 20:43:04 +0800

    bottom navigation finish

--- app/src/main/java/TourismWiz/TourismWiz/ui/TourismWizApp.kt
+++ app/src/main/java/TourismWiz/TourismWiz/ui/TourismWizApp.kt
@@ -2,42 +2,111 @@ package TourismWiz.TourismWiz.ui
 
 import TourismWiz.TourismWiz.R
 import TourismWiz.TourismWiz.ui.screens.*
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 @Composable
 fun TourismWizApp(modifier: Modifier = Modifier) {
-    Scaffold (
-        modifier = Modifier
-            .fillMaxSize(),
-        topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name))}) }
-    ) {
+    val selectedScreenIndex = remember { mutableStateOf(0) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name)) }) },
+        bottomBar = {
+            BottomAppBar(backgroundColor = Color.White) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = { selectedScreenIndex.value = 0 },
+                        modifier = Modifier.size(24.dp) // 設定IconButton的大小為24dp
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.hotel),
+                            contentDescription = "按鈕3"
+                        )
+                    }
+
+                    IconButton(
+                        onClick = { selectedScreenIndex.value = 1 },
+                        modifier = Modifier.size(24.dp) // 設定IconButton的大小為24dp
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.ferriswheel),
+                            contentDescription = "按鈕3"
+                        )
+                    }
+
+                    IconButton(
+                        onClick = { selectedScreenIndex.value = 2 },
+                        modifier = Modifier.size(24.dp) // 設定IconButton的大小為24dp
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.restaurant),
+                            contentDescription = "按鈕3"
+                        )
+                    }
+
+                    IconButton(
+                        onClick = { selectedScreenIndex.value = 2 },
+                        modifier = Modifier.size(24.dp) // 設定IconButton的大小為24dp
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.user),
+                            contentDescription = "按鈕3"
+                        )
+                    }
+                }
+            }
+        }
+    ) { padding ->
         Surface(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it),
+                .padding(padding),
             color = MaterialTheme.colors.background
         ) {
-            val restaurantViewModel : RestaurantViewModel = viewModel(factory = RestaurantViewModel.Factory)
-            RestaurantScreen(
-                restaurantUiState = restaurantViewModel.restaurantUiState,
-                retryAction = restaurantViewModel::getRestaurants
-            )
-//            val hotelViewModel : HotelViewModel = viewModel(factory = HotelViewModel.Factory)
-//            HotelScreen(
-//                hotelUiState = hotelViewModel.hotelUiState,
-//                retryAction = hotelViewModel::getHotels
-//            )
-//            val scenicSpotViewModel : ScenicSpotViewModel = viewModel(factory = ScenicSpotViewModel.Factory)
-//            ScenicSpotScreen(
-//                scenicSpotUiState = scenicSpotViewModel.scenicSpotUiState,
-//                retryAction = scenicSpotViewModel::getScenicSpots
-//            )
+            when (selectedScreenIndex.value) {
+                0 -> {
+                    val hotelViewModel: HotelViewModel = viewModel(factory = HotelViewModel.Factory)
+                    HotelScreen(
+                        hotelUiState = hotelViewModel.hotelUiState,
+                        retryAction = hotelViewModel::getHotels
+                    )
+                }
+                1 -> {
+                    val scenicSpotViewModel: ScenicSpotViewModel =
+                        viewModel(factory = ScenicSpotViewModel.Factory)
+                    ScenicSpotScreen(
+                        scenicSpotUiState = scenicSpotViewModel.scenicSpotUiState,
+                        retryAction = scenicSpotViewModel::getScenicSpots
+                    )
+                }
+                2 -> {
+                    val restaurantViewModel: RestaurantViewModel =
+                        viewModel(factory = RestaurantViewModel.Factory)
+                    RestaurantScreen(
+                        restaurantUiState = restaurantViewModel.restaurantUiState,
+                        retryAction = restaurantViewModel::getRestaurants
+                    )
+                }
+            }
         }
     }
 }
